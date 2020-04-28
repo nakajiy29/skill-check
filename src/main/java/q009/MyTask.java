3994f5cda1c30eacae9e2500a4ba8d343a6268fe
ゆ <yu@Ys-MBA.local>
2020-04-28 10:35:05 +0900

    チェック9を作成

--- src/main/java/q009/MyTask.java
+++ src/main/java/q009/MyTask.java
@@ -1,17 +1,58 @@
 package q009;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyTask implements Runnable {
+    static List<Integer> list = new ArrayList<>();
+
     @Override
     public void run() {
-        int count = 0;
-        Q009.getValue();
-        for (int i = 0; i < 5; i ++) {
-            try {
-                count++;
-                Thread.sleep(500L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        int[] primeNumberArray = calc();
+        print(primeNumberArray);
+    }
+
+    /**
+     * 素因数分解の計算
+     *
+     * @return
+     */
+    public static int[] calc() {
+        int n = Q009.getValue();
+        int m = Q009.getValue();
+        list = new ArrayList<>();
+        BigInteger bi = null;
+        int i = 2;
+        while (!(n <= 1 || m <= i)) {
+            if ((n % i) == 0) {
+                n = n / i;
+                list.add(i);
+            } else {
+                bi = new BigInteger(String.valueOf(i));
+                i = bi.nextProbablePrime().intValue();
             }
         }
+        if (m == n) {
+            System.out.println(n);
+        }
+        // 取り出した素数を配列に入れ直す
+        int[] primeNumberArray = new int[list.size()];
+        for (int j = 0; j < list.size(); j++) {
+            primeNumberArray[j] = list.get(j);
+        }
+        return primeNumberArray;
+    }
+
+    /**
+     * 素因数分解の結果を出力
+     * @param int[] primeNumberArray
+     */
+    static void print(int[] primeNumberArray) {
+        System.out.print(Q009.getValue() + ": ");
+        for (int i = 0; i < primeNumberArray.length - 1; i++) {
+            System.out.print(primeNumberArray[i] + ",");
+        }
+        System.out.println(primeNumberArray[primeNumberArray.length - 1]);
     }
 }
--- src/main/java/q009/Q009.java
+++ src/main/java/q009/Q009.java
@@ -5,52 +5,59 @@ import java.util.Scanner;
 /**
  * Q009 重い処理を別スレッドで実行
  *
- * 標準入力から整数を受け取り、別スレッドで素因数分解して、その整数を構成する2以上の素数を求めるプログラムを記述してください。
- * - 素因数分解は重い処理であるため、別スレッドで実行してください
- * - 標準入力から整数を受け取った後は、再度標準入力に戻ります
- * - 空文字が入力された場合は、素因数分解を実行中の状態を表示します（「実行中」あるいは処理結果を表示）
- * - 素因数分解の効率的なアルゴリズムを求めるのが問題ではないため、あえて単純なアルゴリズムで良い（遅いほどよい）
- *   （例えば、2および3以上の奇数で割り切れるかを全部チェックする方法でも良い）
- * - BigIntegerなどを使って、大きな数値も扱えるようにしてください
-[実行イメージ]
-入力) 65536
-入力)
-65536: 実行中  <-- もし65536の素因数分解が実行中だった場合はこのように表示する
-入力) 12345
-入力)
-65536: 2      <-- 実行が終わっていたら結果を表示する。2の16乗だが、16乗の部分の表示は不要（複数溜まっていた場合の順番は問わない）
-12345: 実行中
-入力)
-12345: 3,5,823
+ * 標準入力から整数を受け取り、別スレッドで素因数分解して、その整数を構成する2以上の素数を求めるプログラムを記述してください。 -
+ * 素因数分解は重い処理であるため、別スレッドで実行してください - 標準入力から整数を受け取った後は、再度標準入力に戻ります -
+ * 空文字が入力された場合は、素因数分解を実行中の状態を表示します（「実行中」あるいは処理結果を表示） -
+ * 素因数分解の効率的なアルゴリズムを求めるのが問題ではないため、あえて単純なアルゴリズムで良い（遅いほどよい）
+ * （例えば、2および3以上の奇数で割り切れるかを全部チェックする方法でも良い） - BigIntegerなどを使って、大きな数値も扱えるようにしてください
+ * [実行イメージ] 入力) 65536 入力) 65536: 実行中 <-- もし65536の素因数分解が実行中だった場合はこのように表示する 入力)
+ * 12345 入力) 65536: 2 <--
+ * 実行が終わっていたら結果を表示する。2の16乗だが、16乗の部分の表示は不要（複数溜まっていた場合の順番は問わない） 12345: 実行中 入力)
+ * 12345: 3,5,823
  */
 public class Q009 {
-    private static String value;
+    private static int value;
+    private static Thread myTask;
+
+    /**
+     * main method
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
         System.out.println("入力) ");
         Scanner s = new Scanner(System.in);
+        boolean isfirst = true;
         try {
-            Thread myTask = new Thread(new MyTask());
-            myTask.start();
             while (s.hasNext()) {
-                System.out.println("入力) ");
-                value = s.next();
-                System.out.println("isAlive: " + myTask.isAlive());
-                if (myTask.isAlive()) {
-                    System.out.println(value + ": 実行中");
-                    Thread.sleep(500L);
-                } else {
-                    System.out.println(value + ": 実行中ではない");
+                value = Integer.valueOf(s.next());
+                if (isfirst) {
                     myTask = new Thread(new MyTask());
                     myTask.start();
+                    isfirst = false;
+                } else {
+                    if (myTask.isAlive()) {
+                        System.out.println(value + ": 実行中");
+                        Thread.sleep(500L);
+                    } else {
+                        myTask = new Thread(new MyTask());
+                        myTask.start();
+                    }
                 }
+                System.out.println("入力) ");
+
             }
         } finally {
             s.close();
         }
     }
 
-    public static String getValue() {
+    /**
+     * 入力された値を取得
+     * @return int 入力された値
+     */
+    public static int getValue() {
         return value;
     }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 2時間 00分
